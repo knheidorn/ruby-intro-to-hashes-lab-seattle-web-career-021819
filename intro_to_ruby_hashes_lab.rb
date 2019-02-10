aa28aa84705f8835af58e44e5a3b6ae22249668f
Kim Heidorn <heidorkn@gmail.com>
2019-02-10 19:13:11 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -7,14 +7,12 @@ def actor
 end
 
 def monopoly
-	monopoly = {}
-  monopoly[:railroads] = {}
-  monopoly
+	monopoly = {railroads: {}}
 end
 
 def monopoly_with_second_tier
-  monopoly = { railroads: {pieces: 4, names: {}, rent_in_dollar: {}
-   }
+  monopoly = {
+    railroads: {pieces: 4, names: {}, rent_in_dollars: {}}
   }
 end
 
